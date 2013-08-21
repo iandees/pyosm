c428e4795e1b18d0db51fd302f5a09b05bce72de
Ian Dees <ian.dees@gmail.com>
2013-08-21 15:56:22 -0500

    Split data into million-object files.

--- examples/osm2postgresql.py
+++ examples/osm2postgresql.py
@@ -5,16 +5,6 @@ import unicodecsv
 import gzip
 import re
 
-changesets_csv = unicodecsv.DictWriter(gzip.GzipFile('changesets.csv.gz', 'w'), ['id', 'created_at', 'closed_at', 'user', 'uid', 'tags', 'bbox'])
-nodes_csv = unicodecsv.DictWriter(gzip.GzipFile('nodes.csv.gz', 'w'), ['id', 'version', 'changeset', 'user', 'uid', 'visible', 'timestamp', 'tags', 'loc'])
-ways_csv = unicodecsv.DictWriter(gzip.GzipFile('ways.csv.gz', 'w'), ['id', 'version', 'changeset', 'user', 'uid', 'visible', 'timestamp', 'tags', 'nds', 'line'])
-relations_csv = unicodecsv.DictWriter(gzip.GzipFile('relations.csv.gz', 'w'), ['id', 'version', 'changeset', 'user', 'uid', 'visible', 'timestamp', 'tags', 'members'])
-
-changesets_csv.writeheader()
-nodes_csv.writeheader()
-ways_csv.writeheader()
-relations_csv.writeheader()
-
 n = 0
 nodes = 0
 nodes_buffer = []
@@ -24,6 +14,24 @@ relations = 0
 relations_buffer = []
 changesets = 0
 changesets_buffer = []
+
+size_of_buffer = 1000
+size_of_slice = 1000000
+
+changesets_gz = gzip.GzipFile('changesets.csv.%05d.gz' % (ways / size_of_slice), 'w')
+changesets_csv = unicodecsv.DictWriter(changesets_gz, ['id', 'created_at', 'closed_at', 'user', 'uid', 'tags', 'bbox'])
+nodes_gz = gzip.GzipFile('nodes.csv.%05d.gz' % (nodes / size_of_slice), 'w')
+nodes_csv = unicodecsv.DictWriter(nodes_gz, ['id', 'version', 'changeset', 'user', 'uid', 'visible', 'timestamp', 'tags', 'loc'])
+ways_gz = gzip.GzipFile('ways.csv.%05d.gz' % (ways / size_of_slice), 'w')
+ways_csv = unicodecsv.DictWriter(ways_gz, ['id', 'version', 'changeset', 'user', 'uid', 'visible', 'timestamp', 'tags', 'nds', 'line'])
+relations_gz = gzip.GzipFile('relations.csv.%05d.gz' % (relations / size_of_slice), 'w')
+relations_csv = unicodecsv.DictWriter(relations_gz, ['id', 'version', 'changeset', 'user', 'uid', 'visible', 'timestamp', 'tags', 'members'])
+
+changesets_csv.writeheader()
+nodes_csv.writeheader()
+ways_csv.writeheader()
+relations_csv.writeheader()
+
 sys.stdout.write('%8d changesets, %10d nodes, %8d ways, %5d relations' % (changesets, nodes, ways, relations))
 for p in iter_osm_file(open(sys.argv[1], 'r'), parse_timestamps=False):
 
@@ -42,9 +50,14 @@ for p in iter_osm_file(open(sys.argv[1], 'r'), parse_timestamps=False):
         nodes_buffer.append(data)
         nodes += 1
 
-        if nodes % 1000 == 0:
+        if nodes % size_of_buffer == 0:
             nodes_csv.writerows(nodes_buffer)
             nodes_buffer = []
+
+        if nodes % size_of_slice == 0:
+            nodes_gz.close()
+            nodes_gz = gzip.GzipFile('nodes.csv.%05d.gz' % (nodes / size_of_slice), 'w')
+            nodes_csv = unicodecsv.DictWriter(nodes_gz, ['id', 'version', 'changeset', 'user', 'uid', 'visible', 'timestamp', 'tags', 'loc'])
 
     elif type(p) == pyosm.model.Way:
         data = {
@@ -60,9 +73,15 @@ for p in iter_osm_file(open(sys.argv[1], 'r'), parse_timestamps=False):
         ways_buffer.append(data)
         ways += 1
 
-        if ways % 1000 == 0:
+        if ways % size_of_buffer == 0:
             ways_csv.writerows(ways_buffer)
             ways_buffer = []
+
+        if ways % size_of_slice == 0:
+            ways_gz.close()
+            ways_gz = gzip.GzipFile('ways.csv.%05d.gz' % (ways / size_of_slice), 'w')
+            ways_csv = unicodecsv.DictWriter(ways_gz, ['id', 'version', 'changeset', 'user', 'uid', 'visible', 'timestamp', 'tags', 'nds', 'line'])
+
     elif type(p) == pyosm.model.Relation:
         data = {
             'id': p.id,
@@ -77,9 +96,15 @@ for p in iter_osm_file(open(sys.argv[1], 'r'), parse_timestamps=False):
         relations_buffer.apend(data)
         relations += 1
 
-        if relations % 1000 == 0:
+        if relations % size_of_buffer == 0:
             relations_csv.writerows(relations_buffer)
             relations_buffer = []
+
+        if relations % size_of_slice == 0:
+            relations_gz.close()
+            relations_gz = gzip.GzipFile('relations.csv.%05d.gz' % (relations / size_of_slice), 'w')
+            relations_csv = unicodecsv.DictWriter(relations_gz, ['id', 'version', 'changeset', 'user', 'uid', 'visible', 'timestamp', 'tags', 'members'])
+
     elif type(p) == pyosm.model.Changeset:
         data = {
             'id': p.id,
@@ -95,24 +120,34 @@ for p in iter_osm_file(open(sys.argv[1], 'r'), parse_timestamps=False):
         changesets_buffer.append(data)
         changesets += 1
 
-        if changesets % 1000 == 0:
+        if changesets % size_of_buffer == 0:
             changesets_csv.writerows(changesets_buffer)
             changesets_buffer = []
+
+        if changesets % size_of_slice == 0:
+            changesets_gz.close()
+            changesets_gz = gzip.GzipFile('changesets.csv.%05d.gz' % (changesets / size_of_slice), 'w')
+            changesets_csv = unicodecsv.DictWriter(changesets_gz, ['id', 'created_at', 'closed_at', 'user', 'uid', 'tags', 'bbox'])
+
     data = None
     n += 1
 
-    if n % 1000 == 0:
+    if n % size_of_buffer == 0:
         sys.stdout.write('\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b')
         sys.stdout.write('%8d changesets, %10d nodes, %8d ways, %5d relations' % (changesets, nodes, ways, relations))
         sys.stdout.flush()
 
 changesets_csv.writerows(changesets_buffer)
+changesets_gz.close()
 changesets_buffer = []
 nodes_csv.writerows(nodes_buffer)
+nodes_gz.close()
 nodes_buffer = []
 ways_csv.writerows(ways_buffer)
+ways_gz.close()
 ways_buffer = []
 relations_csv.writerows(relations_buffer)
+relations_gz.close()
 relations_buffer = []
 
 
