20ed8721dd6fb6921a2b95481778702574b5533d
Ian Dees <ian.dees@gmail.com>
2013-09-29 19:46:42 -0500

    Move the osmc parse out into a separate method.

--- pyosm/parsing.py
+++ pyosm/parsing.py
@@ -110,6 +110,85 @@ def iter_changeset_stream(start_sqn=None, base_url='http://planet.openstreetmap.
         sequenceNumber += 1
 
 
+def iter_osm_change_file(f, parse_timestamps=True):
+    action = None
+    obj = None
+    for event, elem in etree.iterparse(f, events=('start', 'end')):
+        if event == 'start':
+            if elem.tag == 'node':
+                obj = model.Node(
+                    int(elem.attrib['id']),
+                    int(elem.attrib['version']),
+                    int(elem.attrib['changeset']),
+                    elem.attrib.get('user'),
+                    maybeInt(elem.attrib.get('uid')),
+                    maybeBool(elem.attrib.get('visible')),
+                    isoToDatetime(elem.attrib.get('timestamp')) if parse_timestamps else elem.attrib.get('timestamp'),
+                    float(elem.attrib['lat']),
+                    float(elem.attrib['lon']),
+                    []
+                )
+            elif elem.tag == 'way':
+                obj = model.Way(
+                    int(elem.attrib['id']),
+                    int(elem.attrib['version']),
+                    int(elem.attrib['changeset']),
+                    elem.attrib.get('user'),
+                    maybeInt(elem.attrib.get('uid')),
+                    maybeBool(elem.attrib.get('visible')),
+                    isoToDatetime(elem.attrib.get('timestamp')) if parse_timestamps else elem.attrib.get('timestamp'),
+                    [],
+                    []
+                )
+            elif elem.tag == 'tag':
+                obj.tags.append(
+                    model.Tag(
+                        elem.attrib['k'],
+                        elem.attrib['v']
+                    )
+                )
+            elif elem.tag == 'nd':
+                obj.nds.append(int(elem.attrib['ref']))
+            elif elem.tag == 'relation':
+                obj = model.Relation(
+                    int(elem.attrib['id']),
+                    int(elem.attrib['version']),
+                    int(elem.attrib['changeset']),
+                    elem.attrib.get('user'),
+                    maybeInt(elem.attrib.get('uid')),
+                    maybeBool(elem.attrib.get('visible')),
+                    isoToDatetime(elem.attrib.get('timestamp')) if parse_timestamps else elem.attrib.get('timestamp'),
+                    [],
+                    []
+                )
+            elif elem.tag == 'member':
+                obj.members.append(
+                    model.Member(
+                        elem.attrib['type'],
+                        int(elem.attrib['ref']),
+                        elem.attrib['role']
+                    )
+                )
+            elif elem.tag in ('create', 'modify', 'delete'):
+                action = elem.tag
+        elif event == 'end':
+            if elem.tag == 'node':
+                yield (action, obj)
+                obj = None
+            elif elem.tag == 'way':
+                yield (action, obj)
+                obj = None
+            elif elem.tag == 'relation':
+                yield (action, obj)
+                obj = None
+            elif elem.tag in ('create', 'modify', 'delete'):
+                action = None
+
+        elem.clear()
+        while elem.getprevious() is not None:
+            del elem.getparent()[0]
+
+
 def iter_osm_stream(start_sqn=None, base_url='http://planet.openstreetmap.org/replication/minute', expected_interval=60, parse_timestamps=True):
     """Start processing an OSM diff stream and yield one changeset at a time to
     the caller."""
@@ -133,82 +212,7 @@ def iter_osm_stream(start_sqn=None, base_url='http://planet.openstreetmap.org/re
         content = StringIO.StringIO(content.read())
         gzipper = gzip.GzipFile(fileobj=content)
 
-        action = None
-        obj = None
-        for event, elem in etree.iterparse(gzipper, events=('start', 'end')):
-            if event == 'start':
-                if elem.tag == 'node':
-                    obj = model.Node(
-                        int(elem.attrib['id']),
-                        int(elem.attrib['version']),
-                        int(elem.attrib['changeset']),
-                        elem.attrib.get('user'),
-                        maybeInt(elem.attrib.get('uid')),
-                        maybeBool(elem.attrib.get('visible')),
-                        isoToDatetime(elem.attrib.get('timestamp')) if parse_timestamps else elem.attrib.get('timestamp'),
-                        float(elem.attrib['lat']),
-                        float(elem.attrib['lon']),
-                        []
-                    )
-                elif elem.tag == 'way':
-                    obj = model.Way(
-                        int(elem.attrib['id']),
-                        int(elem.attrib['version']),
-                        int(elem.attrib['changeset']),
-                        elem.attrib.get('user'),
-                        maybeInt(elem.attrib.get('uid')),
-                        maybeBool(elem.attrib.get('visible')),
-                        isoToDatetime(elem.attrib.get('timestamp')) if parse_timestamps else elem.attrib.get('timestamp'),
-                        [],
-                        []
-                    )
-                elif elem.tag == 'tag':
-                    obj.tags.append(
-                        model.Tag(
-                            elem.attrib['k'],
-                            elem.attrib['v']
-                        )
-                    )
-                elif elem.tag == 'nd':
-                    obj.nds.append(int(elem.attrib['ref']))
-                elif elem.tag == 'relation':
-                    obj = model.Relation(
-                        int(elem.attrib['id']),
-                        int(elem.attrib['version']),
-                        int(elem.attrib['changeset']),
-                        elem.attrib.get('user'),
-                        maybeInt(elem.attrib.get('uid')),
-                        maybeBool(elem.attrib.get('visible')),
-                        isoToDatetime(elem.attrib.get('timestamp')) if parse_timestamps else elem.attrib.get('timestamp'),
-                        [],
-                        []
-                    )
-                elif elem.tag == 'member':
-                    obj.members.append(
-                        model.Member(
-                            elem.attrib['type'],
-                            int(elem.attrib['ref']),
-                            elem.attrib['role']
-                        )
-                    )
-                elif elem.tag in ('create', 'modify', 'delete'):
-                    action = elem.tag
-            elif event == 'end':
-                if elem.tag == 'node':
-                    yield (action, obj)
-                    obj = None
-                elif elem.tag == 'way':
-                    yield (action, obj)
-                    obj = None
-                elif elem.tag == 'relation':
-                    yield (action, obj)
-                    obj = None
-                elif elem.tag in ('create', 'modify', 'delete'):
-                    action = None
-
-            elem.clear()
-            while elem.getprevious() is not None:
-                del elem.getparent()[0]
+        iter_osm_change_file(gzipper, parse_timestamps)
 
         # After parsing the OSC, check to see how much time is remaining
         stateTs = datetime.datetime.strptime(state['timestamp'], "%Y-%m-%dT%H:%M:%SZ")
