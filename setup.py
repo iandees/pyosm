6795c5a823f17e3dac0d854bbb4a392e37505919
Ian Dees <ian.dees@gmail.com>
2013-11-05 14:19:39 -0600

    Adding some keywords for pypi.

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     license='LICENSE.txt',
     description='Parses OSM XML files in a fast and reliable way.',
     long_description=open('README.md').read(),
+    keywords = ['osm', 'openstreetmap', 'xml', 'parsing'],
     install_requires=[
         'lxml==3.2.3'
     ]
