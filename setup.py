64ddbb0bb48dd188c422fcf3bcfd5d1c31ee5737
Ian Dees <ian.dees@gmail.com>
2013-11-10 22:32:39 -0600

    Get ready for 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pyosm',
-    version='0.1',
+    version='0.2',
     author='Ian Dees',
     author_email='ian.dees@gmail.com',
     packages=['pyosm'],
